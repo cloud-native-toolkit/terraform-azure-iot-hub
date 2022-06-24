084677bfa098205d12ddb83856dbffbbff309968
Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>
2022-06-24 10:47:18 -0400

    Updated variables.tf
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -1,50 +1,30 @@
 
 # Resource Group Variables
+# Resource Group Variables
 variable "resource_group_name" {
   type        = string
-  description = "Existing resource group where the IKS cluster will be provisioned."
-}
-
-variable "ibmcloud_api_key" {
-  type        = string
-  description = "The api key for IBM Cloud access"
+  description = "The name of the IBM Cloud resource group where the VPC instance will be created."
 }
 
 variable "region" {
   type        = string
-  description = "Region for VLANs defined in private_vlan_number and public_vlan_number."
+  description = "The IBM Cloud region where the cluster will be/has been installed."
 }
 
-variable "namespace" {
+variable "name" {
   type        = string
-  description = "Namespace for tools"
-}
-
-variable "cluster_name" {
-  type        = string
-  description = "The name of the cluster"
+  description = "The name of the vpc instance"
   default     = ""
-}
-
-variable "cluster_type" {
-  type        = string
-  description = "The type of cluster that should be created (openshift or kubernetes)"
-}
-
-variable "cluster_exists" {
-  type        = string
-  description = "Flag indicating if the cluster already exists (true or false)"
-  default     = "true"
 }
 
 variable "name_prefix" {
   type        = string
-  description = "Prefix name that should be used for the cluster and services. If not provided then resource_group_name will be used"
+  description = "The name of the vpc resource"
   default     = ""
 }
 
-variable "vpc_cluster" {
+variable "provision" {
   type        = bool
-  description = "Flag indicating that this is a vpc cluster"
-  default     = false
+  description = "Flag indicating that the instance should be provisioned. If false then an existing instance will be looked up"
+  default     = true
 }
