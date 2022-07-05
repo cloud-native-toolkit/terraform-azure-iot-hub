ec0bacb93f027aa917f9696d698fdffaf12e3059
Vamsy Sivaarcca <vamsysivaarcca@vamsys-mbp.lan>
2022-07-05 08:52:01 -0400

    Updated stage2
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@vamsys-mbp.lan>

--- test/stages/stage2-iot-hub.tf
+++ test/stages/stage2-iot-hub.tf
@@ -4,6 +4,17 @@ module "iot-hub" {
 
     resource_group_name = module.resource_group_name
     storage_account_name = module.storage_account.name
+    iothub_id = var.iothub_id
+    iothub_name = var.iothub_name
+    servicebus_queue_authorization_rule_name = var.servicebus_queue_authorization_rule_name
+    region = var.region
+    servicebus_namespace = var.servicebus_namespace
+    eventhub_name = var.eventhub_name
+    servicebus_queue_id = var.servicebus_queue_id
+    sku_name = var.sku_name
+    servicebus_endpoint_queue_name = var.servicebus_endpoint_queue_name
+    servicebus_namespace_id = var.servicebus_namespace_id
+    namespace = var.namespace
     sku = var.sku
     servicebus_queue_name = var.servicebus_queue_name
     iothub_route_name = var.iothub_route_name
