2416abaff8de1393e4c6bb0096e285257444e566
Vamsy Sivaarcca <vamsysivaarcca@vamsys-mbp.lan>
2022-07-10 18:08:38 -0400

    Updated stage2
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@vamsys-mbp.lan>

--- test/stages/stage2-iot-hub.tf
+++ test/stages/stage2-iot-hub.tf
@@ -4,6 +4,8 @@ module "iot-hub" {
 
     resource_group_name = var.resource_group_name
     storage_account_name = var.storage_account_name
+    container_name = var.container_name
+    sku_capacity = var.sku_capacity
     iothub_id = var.iothub_id
     iothub_name = var.iothub_name
     iothub_endpoint_storage_container_name = var.iothub_endpoint_storage_container_name
