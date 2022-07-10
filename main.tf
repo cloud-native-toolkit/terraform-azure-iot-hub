c8d12170c4448114cb17944b06572ca9d9dab9f2
Vamsy Sivaarcca <vamsysivaarcca@vamsys-mbp.lan>
2022-07-10 18:12:40 -0400

    Updated main.tf
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@vamsys-mbp.lan>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name.default
+  name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.region
   account_tier             = var.storage_account_tier
