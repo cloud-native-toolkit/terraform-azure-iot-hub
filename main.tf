064fdb154da15e6e78a6301ec6df21d41d7ceec5
Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>
2022-08-09 23:03:07 -0400

    Updated main and stage files
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>

--- main.tf
+++ main.tf
@@ -1,35 +1,64 @@
-resource "azurerm_storage_account" "storage_account" {
-  resource_group_name      = var.resource_group_name
+resource "azurerm_resource_group" {
+  name     = var.resource_group_name
+  location = var.region
+}
+
+
+resource "azurerm_storage_account" {
   name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
   location                 = var.region
-  account_tier             = var.storage_account_tier
-  account_replication_type = var.replication_type
+  # account_tier             = "Standard"
+  # account_replication_type = "LRS"
 }
 
 
-resource "azurerm_storage_container" "storage_container" {
-  count                 = var.container_name != "" ? 1 : 0
-  name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.storage_account.name
-  container_access_type = var.container_access_type
+resource "azurerm_storage_container" {
+  name                  = ""
+   storage_account_name  = var.storage_account_name
+  # container_access_type = "private"
 }
 
 
-resource "azurerm_servicebus_namespace" "servicebus_namespace" {
-  name                = var.servicebus_namespace
+resource "azurerm_eventhub_namespace" {
+  name                = ""
+  resource_group_name = var.resource_group_name
+  location            = var.region
+  sku                 = "Basic"
+  }
+
+
+resource "azurerm_eventhub" {
+  name                = ""
+  resource_group_name = var.resource_group_name
+  namespace_name      = var.namespace
+  partition_count     = 2
+  message_retention   = 1
+}
+
+resource "azurerm_eventhub_authorization_rule"  {
+  resource_group_name = var.resource_group_name
+  namespace_name      = var.namespace
+  eventhub_name       = var.eventhub_name
+  name                = ""
+  send                = true
+}
+
+resource "azurerm_servicebus_namespace" {
+  name                = ""
   location            = var.region
   resource_group_name = var.resource_group_name
-  sku                 = var.sku
+  sku                 = "Standard"
 }
 
-resource "azurerm_servicebus_queue" "servicebus_queue" {
-  name         = var.servicebus_queue_name
+resource "azurerm_servicebus_queue" {
+  name         = ""
   namespace_id = var.servicebus_namespace_id
   enable_partitioning = true
 }
 
-resource "azurerm_servicebus_queue_authorization_rule" "servicebus_queue_authorization_rule" {
-  name     = var.servicebus_queue_authorization_rule_name
+resource "azurerm_servicebus_queue_authorization_rule"  {
+  name     = ""
   queue_id = var.servicebus_queue_id
 
   listen = false
@@ -37,46 +66,41 @@ resource "azurerm_servicebus_queue_authorization_rule" "servicebus_queue_authori
   manage = false
 }
 
-resource "azurerm_iothub" "iothub" {
+resource "azurerm_iothub"  {
   name                = var.iothub_name
   resource_group_name = var.resource_group_name
   location            = var.region
 
   sku {
-    name     = var.sku_name
-    capacity = var.sku_capacity
-  }
-  tags = var.tags
+    name     = "S1"
+    capacity = "1"
+    }
+  tags = {
+    purpose = "testing"
+    }
 }
 
-resource "azurerm_iothub_endpoint_storage_container" "endpoint_storage_container" {
+resource "azurerm_iothub_endpoint_storage_container" {
   resource_group_name = var.resource_group_name
   iothub_id           = var.iothub_id
-  name                = var.iothub_endpoint_storage_container_name
+  name                = ""
 
-  connection_string          = azurerm_storage_account.storage_account.primary_blob_connection_string
+  connection_string          = var.storage_account_name.connection_string
   batch_frequency_in_seconds = 60
   max_chunk_size_in_bytes    = 10485760
-  container_name             = var.container_name
+  container_name             = ""
   encoding                   = "Avro"
   file_name_format           = "{iothub}/{partition}_{YYYY}_{MM}_{DD}_{HH}_{mm}"
 }
 
-resource "azurerm_iothub_route" "iothub_route" {
+resource "azurerm_iothub_route" {
   resource_group_name = var.resource_group_name
   iothub_name         = var.iothub_name
-  name                = var.iothub_route_name
+  name                = ""
 
   source         = "DeviceMessages"
   condition      = "true"
-  endpoint_names = [azurerm_iothub_endpoint_storage_container.endpoint_storage_container.name]
+  endpoint_names = [azurerm_iothub_endpoint_storage_container.example.name]
   enabled        = true
 }
 
-resource "azurerm_iothub_endpoint_servicebus_queue" "iothub_endpoint_servicebus_queue" {
-  resource_group_name = var.resource_group_name
-  iothub_id           = var.iothub_id
-  name                = var.servicebus_endpoint_queue_name
-
-  connection_string = azurerm_servicebus_queue_authorization_rule.servicebus_queue_authorization_rule.primary_connection_string
-}
